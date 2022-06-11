2ab0936eafdb926d93ca8b2b2c8e12562b0642f4
ichou <ichou.bahba@gmail.com>
2022-06-11 03:20:41 +0100

    add settings components
    general, access, integration, labels, notifications and UOM

--- src/app/modules/settings/setting.module.ts
+++ src/app/modules/settings/setting.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingRoutingModule } from './setting-routing.module';
+import { GeneralComponent } from './components/general/general.component';
+import { AccessComponent } from './components/access/access.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { LabelsComponent } from './components/labels/labels.component';
+import { UOMComponent } from './components/uom/uom.component';
+import { IntegrationComponent } from './components/integration/integration.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GeneralComponent,
+    AccessComponent,
+    NotificationsComponent,
+    LabelsComponent,
+    UOMComponent,
+    IntegrationComponent
+  ],
   imports: [
     CommonModule,
     SettingRoutingModule
