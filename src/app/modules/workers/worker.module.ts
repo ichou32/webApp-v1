1e4327c98d8d7a1b563c3b949853e357bd746ab7
ichou <ichou.bahba@gmail.com>
2022-06-11 03:19:44 +0100

    add worker components
    add-worker

--- src/app/modules/workers/worker.module.ts
+++ src/app/modules/workers/worker.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WorkerRoutingModule } from './worker-routing.module';
+import { AddWorkerComponent } from './components/add-worker/add-worker.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddWorkerComponent
+  ],
   imports: [
     CommonModule,
     WorkerRoutingModule
