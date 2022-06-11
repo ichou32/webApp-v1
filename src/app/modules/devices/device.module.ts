2abc0e782aabd9522e7e1138da8ad001d93aed8e
ichou <ichou.bahba@gmail.com>
2022-06-11 03:25:09 +0100

    add device's component

--- src/app/modules/devices/device.module.ts
+++ src/app/modules/devices/device.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DeviceRoutingModule } from './device-routing.module';
+import { AddDeviceComponent } from './components/add-device/add-device.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddDeviceComponent
+  ],
   imports: [
     CommonModule,
     DeviceRoutingModule
