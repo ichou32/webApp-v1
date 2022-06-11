55d1dcdacf257a12278920b6a9011de4983db089
ichou <ichou.bahba@gmail.com>
2022-06-11 03:23:13 +0100

    add job's components
    destination, order, overview, sender and Pod

--- src/app/modules/jobs/job.module.ts
+++ src/app/modules/jobs/job.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JobRoutingModule } from './job-routing.module';
+import { OverviewComponent } from './components/overview/overview.component';
+import { PodComponent } from './components/pod/pod.component';
+import { DestinationComponent } from './components/destination/destination.component';
+import { SenderComponent } from './components/sender/sender.component';
+import { OrderComponent } from './components/order/order.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OverviewComponent,
+    PodComponent,
+    DestinationComponent,
+    SenderComponent,
+    OrderComponent
+  ],
   imports: [
     CommonModule,
     JobRoutingModule
