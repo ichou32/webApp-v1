f3ce092ba99cccd5b6e4747dd1a89f3e00e4d92a
ichou <ichou.bahba@gmail.com>
2022-06-11 04:18:01 +0100

    add shared componensts
    home, sidebar, header and error components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SideBarComponent } from './components/menu/side-bar/side-bar.component';
+import { PageNotFoundComponent } from './components/errors/page-not-found/page-not-found.component';
+import { HeaderComponent } from './components/menu/header/header.component';
+import { HomeComponent } from './components/menu/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    SideBarComponent,
+    PageNotFoundComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
