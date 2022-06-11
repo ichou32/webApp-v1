80aa3e630e6c11e2231e8459bc0a85170b87f8f7
ichou <ichou.bahba@gmail.com>
2022-06-11 03:19:18 +0100

    add depot's components
    add-depot

--- src/app/modules/depots/depots.module.ts
+++ src/app/modules/depots/depots.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DepotsRoutingModule } from './depots-routing.module';
+import { AddDepotComponent } from './components/add-depot/add-depot.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddDepotComponent
+  ],
   imports: [
     CommonModule,
     DepotsRoutingModule
