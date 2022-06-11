34bf795b9bac06616749017daec271c30f9ef8ec
ichou <ichou.bahba@gmail.com>
2022-06-11 03:26:04 +0100

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillingModule } from './modules/billing/billing.module';
+import { DepotsModule } from './modules/depots/depots.module';
+import { DeviceModule } from './modules/devices/device.module';
+import { DispatcherModule } from './modules/dispatchers/dispatcher.module';
+import { FleetModule } from './modules/fleets/fleet.module';
+import { GeofenceModule } from './modules/geofences/geofence.module';
+import { GroupModule } from './modules/groups/group.module';
+import { JobModule } from './modules/jobs/job.module';
+import { MerchantModule } from './modules/merchants/merchant.module';
+import { ProviderModule } from './modules/providers/provider.module';
+import { SettingModule } from './modules/settings/setting.module';
+import { WorkerModule } from './modules/workers/worker.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +22,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BillingModule,
+    DepotsModule,
+    DeviceModule,
+    DispatcherModule,
+    FleetModule,
+    GeofenceModule,
+    GroupModule,
+    JobModule,
+    MerchantModule,
+    ProviderModule,
+    SettingModule,
+    WorkerModule,
     AppRoutingModule
   ],
   providers: [],
