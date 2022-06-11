324d17176c0221f0f0fe5f71705d43e1ca5f40a9
ichou <ichou.bahba@gmail.com>
2022-06-11 03:21:44 +0100

    add merchant's components
    add-merchant

---
+++ src/app/modules/merchants/components/add-merchant/add-merchant.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddMerchantComponent } from './add-merchant.component';
+
+describe('AddMerchantComponent', () => {
+  let component: AddMerchantComponent;
+  let fixture: ComponentFixture<AddMerchantComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddMerchantComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddMerchantComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/modules/merchants/merchant.module.ts
+++ src/app/modules/merchants/merchant.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MerchantRoutingModule } from './merchant-routing.module';
+import { AddMerchantComponent } from './components/add-merchant/add-merchant.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddMerchantComponent
+  ],
   imports: [
     CommonModule,
     MerchantRoutingModule
